a2528d25d64c129eb5effc394ba1e3e0bf4a95ea
martin gomez <mgomez@syncromind.com>
2023-11-24 10:28:22 -0300

    Random facts

--- projects/Fetching/src/App.jsx
+++ projects/Fetching/src/App.jsx
@@ -2,39 +2,44 @@ import { useEffect, useState } from "react"
 import '../src/style.css'
  
 const catEndPointFact = 'https://catfact.ninja/fact';
-const imagePrefix = 'https://cataas.com/cat';
+const imagePrefix = 'https://cataas.com/';
 
 export function App() {
     const [fact, setFact] = useState()
     const [imageUrl, setImageUrl] = useState()
 
+    const getRandomFact = () => {
+        fetch(catEndPointFact)
+            .then(res => res.json())
+            .then(data => {
+                const { fact } = data
+                setFact(fact)
+            })
+    }
+
+    useEffect(getRandomFact, [])
 
     useEffect(() => {
-        fetch(catEndPointFact)
-        .then(res => res.json())   
-        .then(data => {
-            const { fact } = data;
-            setFact(fact)      
-    })         
-    }, [])
-    
-    useEffect(() => {
         if (!fact) return
-        const firstWords = fact.split(' ', 3).join(' ')
-        fetch(`https://cataas.com/cat/says/${firstWords}?size=50&color=red&json=true`)
+        const threeFirstWords = fact.split(' ', 3).join(' ')
+        fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)  
         .then(res => res.json())
         .then(response => {
             const { url } = response
             setImageUrl(url)
-        })
+        })  
     }, [fact])
 
+    const handleClick = () => {
+        getRandomFact()
+    }       
+    
     return (
         <main>
-        <h1>Example</h1>
+        <h1>Cat fact</h1>
+        <button onClick={handleClick}>Get new fact</button>
         {fact && <p>{fact}</p>}
-        <img src={`${imagePrefix}`} alt={`Image extracted using the first three words for ${fact}`} />
+        {imageUrl && <img src={`${imagePrefix}${threeFirstWords}`} alt={`Image extracted using the first three words for ${fact}`} />}
         </main>
-
     )
 }
