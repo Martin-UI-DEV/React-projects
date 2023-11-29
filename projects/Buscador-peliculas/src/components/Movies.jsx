7b0c26e7f7a57c5bb8613ed1856e524e73fd0913
martin gomez <mgomez@syncromind.com>
2023-11-29 16:26:58 -0300

    Render

---
+++ projects/Buscador-peliculas/src/components/Movies.jsx
@@ -0,0 +1,37 @@
+
+
+// eslint-disable-next-line react/prop-types
+function ListOfMovies ({ movies }) {
+  return (
+    <ul className='movies'>
+      {
+        // eslint-disable-next-line react/prop-types
+        movies.map(movie => (
+          <li className='movie' key={movie.id}>
+            <h3>{movie.title}</h3>
+            <p>{movie.year}</p>
+            <img src={movie.image} alt={movie.title} />
+          </li>
+        ))
+      }
+    </ul>
+  )
+}
+
+function NoMoviesResults () {
+  return (
+    <p>No se encontraron películas para esta búsqueda</p>
+  )
+}
+
+// eslint-disable-next-line react/prop-types
+export function Movies ({ movies }) {
+  // eslint-disable-next-line react/prop-types
+  const hasMovies = movies?.length > 0
+
+  return (
+    hasMovies
+      ? <ListOfMovies movies={movies} />
+      : <NoMoviesResults />
+  )
+}
