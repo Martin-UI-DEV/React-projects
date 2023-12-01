10b839effbaee653a20dd07398de86466d70b0d3
martin gomez <mgomez@syncromind.com>
2023-12-01 11:07:06 -0300

    Error messages

--- projects/Buscador-peliculas/src/App.jsx
+++ projects/Buscador-peliculas/src/App.jsx
@@ -1,21 +1,59 @@
 import './App.css'
+import { useEffect, useState, useRef } from 'react'
 import { useMovies } from './hooks/useMovies.js'
 import { Movies } from './components/Movies.jsx'
 
+function useSearch() {
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === ''
+      return
+    }
+    if (search === '') {
+      setError('Ingrese un nombre válido')
+      return
+    }
+
+    if (search.length < 3) {
+      setError('La búsqueda debe tener al menos 3 caracteres')
+      return
+    }
+
+    setError(null)
+  }, [search])
+
+  return { search, updateSearch, error }
+}
+
 function App() {
 
-const { movies } = useMovies()
+  const { movies } = useMovies()
+  const {search, updateSearch, error} = useSearch()
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log({ search })
+  }
+
+  const handleChange = (event) => {
+    updateSearch(event.target.value)
+  }
 
   return (
     <div className='page'>
 
       <header>
         <h1>Buscador de películas</h1>
-        <form className='form'>
-          <input placeholder='Avengers, Star Wars, The Matrix...'
-          />
+        <form className='form' onSubmit={handleSubmit}>
+          <input className={error ? 'danger' : ''} onChange={handleChange} value={search} name="query" placeholder='Avengers, Star Wars, The Matrix...' />
           <button type='submit'>Buscar</button>
         </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
 
       <main>
