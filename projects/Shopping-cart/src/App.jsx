261d2380476dbac5fe442f69feff231ea44edaae
martin gomez <mgomez@syncromind.com>
2023-12-20 12:43:51 -0300

    useContext & useId

--- projects/Shopping-cart/src/App.jsx
+++ projects/Shopping-cart/src/App.jsx
@@ -1,39 +1,20 @@
 import { products as initialProducts } from './mocks/product.json'
-import { Products } from './components/Products'
+import { Products } from './components/Products.jsx'
 import { useState } from 'react'
-import { Header } from './components/Header'
-import { Footer } from './components/Footer'
-import { IS_DEVELOPMENT } from './config'
+import { Header } from './components/Header.jsx'
+import { Footer } from './components/Footer.jsx'
+import { IS_DEVELOPMENT } from './config.js'
+import { useFilters } from './hooks/useFilters.js'
 
-function useFilters() {
-  const [filters, setFilters] = useState({
-    category: 'all',
-    minPrice: 0
-  })
-
-  const filterProducts = (products) => {
-    return products.filter(product => {
-      return (
-        product.price >= filters.minPrice &&
-        (
-          filters.category === 'all' ||
-          product.category === filters.category
-        )
-      )
-    })
-  }
-
-  return { filters, filterProducts, setFilters}
-}
 
 function App() {
   const [products] = useState(initialProducts)
-  const { filters, filterProducts, setFilters} = useFilters()
+  const { filters, filterProducts} = useFilters()
   const filteredProducts = filterProducts(products)
 
   return (
     <>
-    <Header changeFilters={setFilters} />
+    <Header/>
     <Products products={filteredProducts} />
     {IS_DEVELOPMENT && <Footer filters={filters}/>}
     </>
